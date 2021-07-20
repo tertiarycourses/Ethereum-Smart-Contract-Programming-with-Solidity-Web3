917bff6fbf5b8571b12b03fe7ae9440d430a50e3
Alfred Ang <angch@tertiaryinfotech.com>
2021-07-21 01:26:22 +0800

    210621

--- exercises/module5_2.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.4.0 <0.7.0;
-
-contract C {
-    uint constant x = 32**22 + 8;
-    string constant text = "abc";
-    bytes32 constant myHash = keccak256("abc");
-}
--- exercises/module7_2.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.1;
- 
-interface Token {
-    function transfer(address recipient, uint amount) external;
-    function balanceOf(address _owner) external view returns (uint256 balance);
-}
